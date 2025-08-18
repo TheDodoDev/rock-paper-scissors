d5786d482038ecdbced034480572e333e2fbb7d7
Justin Yang <100032995+TheDodoDev@users.noreply.github.com>
2025-08-18 17:32:31 -0500

    feat: Add score tracking and rounds for full game

--- javascript/game.js
+++ javascript/game.js
@@ -15,28 +15,53 @@ let getComputerChoice = () => {
 let determineWinner = (humanChoice, computerChoice) => {
     if (humanChoice == ROCK) {
         if (computerChoice == SCISSORS) {
+            playerScore++;
             return "You Win! Rocks smashes Scissors!";
         }
         else if (computerChoice == PAPER) {
+            computerScore++;
             return "You Lose! Rock gets smothered by Paper!";
         }
     }
     else if (humanChoice == PAPER) {
         if (computerChoice == ROCK) {
+            playerScore++;
             return "You Win! Paper smothers Rock!";
         }
         else if (computerChoice == SCISSORS) {
+            computerScore++;
             return "You Lose! Paper gets cut by Scissors!";
         }
     }
     else if (humanChoice == SCISSORS) {
         if (computerChoice == PAPER) {
+            playerScore++;
             return "You Win! Scissors cut Paper!";
         }
         else if (computerChoice == ROCK) {
+            computerScore++;
             return "You Lose! Scissors gets smashed by Rock!";
         }
     }
     return "Tie! You both picked the same thing!";
 }
 //Step four: Repeat previous steps for a total of N rounds
+let playerScore = 0;
+let computerScore = 0;
+
+for (let i = 0; i < 5; i++) {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+
+    console.log(determineWinner(humanChoice, computerChoice));
+}
+
+if (playerScore > computerScore) {
+    console.log("You Win the Game!")
+}
+else if (playerScore < computerScore) {
+    console.log("COM Wins the game!");
+}
+else {
+    console.log("Tie Game!");
+}
